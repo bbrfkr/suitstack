2e8c29b57fd1baf276747900235d4a98331ae92d
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2017-04-02 22:48:23 +0900

    openstack_nova_addnodes bugfix

--- Roles/openstack_nova_addnodes/itamae/main.rb
+++ Roles/openstack_nova_addnodes/itamae/main.rb
@@ -3,5 +3,5 @@ require './Modules/blockinfile'
 node.reverse_merge!(defaults_load(__FILE__))
 
 # add compute nodes to cell database
-execute "su -s /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova"
+execute "su -s /bin/sh -c \"nova-manage cell_v2 discover_hosts --verbose\" nova"
 
