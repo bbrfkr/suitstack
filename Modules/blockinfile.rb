ad747c84569a296ea6ea8ea8e6fc5159eb8d3ae6
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2017-04-05 23:05:14 +0900

    blockinfile.rb fix

--- Modules/blockinfile.rb
+++ Modules/blockinfile.rb
@@ -3,7 +3,7 @@ def blockinfile(insertafter,block,marker,content)
   marker_end = "###  #{ marker } END  ###\n"
   insertafter += "\n"
   if not (content =~ /#{ Regexp.escape(marker_start + block + marker_end) }/)
-    content.gsub!(/#{ Regexp.escape(insertafter) }#{ Regexp.escape(marker_start) }.*#{ Regexp.escape(marker_end)}/m, insertafter)
+    content.gsub!(/#{ Regexp.escape(marker_start) }.*#{ Regexp.escape(marker_end)}/m, "")
     content.gsub!(insertafter, insertafter + marker_start + block + marker_end)
   end
 end
