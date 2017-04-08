79944a40879c937fea2675bfe3aa89bc07a5f2ee
Tatsunori Saito <1.boogie.5.sophia.4.pop.0@gmail.com>
2017-04-08 17:33:50 +0900

    openstack_nova_addnodes delete

--- Roles/openstack_nova_addnodes/itamae/main.rb
+++
@@ -1,7 +0,0 @@
-require './Modules/defaults'
-require './Modules/blockinfile'
-node.reverse_merge!(defaults_load(__FILE__))
-
-# add compute nodes to cell database
-execute "su -s /bin/sh -c \"nova-manage cell_v2 discover_hosts --verbose\" nova"
-
